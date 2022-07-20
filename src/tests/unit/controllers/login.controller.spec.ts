f4cfc65bebe2adabac5f10d9418d925cb292533e
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-07-20 12:02:52 -0300

    test(login): login controller and validation fully tested

--- src/tests/unit/controllers/login.controller.spec.ts
+++ src/tests/unit/controllers/login.controller.spec.ts
@@ -1,6 +1,10 @@
 import request from 'supertest';
+import sinon from 'sinon';
 import { expect } from 'chai';
 import app from '../../../app';
+import loginService from '../../../services/loginService';
+import HttpException from '../../../utils/http.exception';
+import HttpStatusCode from '../../../utils/http.status.code';
 
 describe('Test login controller', () => {
   it('Test login without sending data', async () => {
@@ -9,29 +13,38 @@ describe('Test login controller', () => {
       .send({});
     expect(response.status).to.eql(400);
   });
-  it('Test login sending incorrect data', async () => {
+  it('Test login sending incorrect email format', async () => {
     const response = await request(app)
       .post('/login')
-      .send({ email: 'incorretEmailFormat', password: 'short' });
+      .send({ email: 'incorretEmailFormat', password: 'password' });
     expect(response.status).to.eql(422);
   });
-  it('Test login sending correct data for no user exists', async () => {
+  it('Test login sending short password', async () => {
+    const response = await request(app)
+      .post('/login')
+      .send({ email: 'test@test.com', password: 'short' });
+    expect(response.status).to.eql(422);
+  });
+  it('Test login sending invalid credentials', async () => {
+    const stub = sinon.stub(loginService, 'login').throws(new HttpException(HttpStatusCode.UNAUTHORIZED, 'Invalid Credentials'));
     const response = await request(app)
       .post('/login')
       .send({ email: 'teste@teste.com', password: 'newpassword' });
     expect(response.status).to.eql(401);
+    expect(response.body.message).to.eql('Invalid Credentials');
+    stub.restore();
   });
   it('Test login successful', async () => {
-    await request(app)
-      .post('/users')
-      .send({
-        fullName: 'new name',
-        email: 'newteste@teste.com',
-        password: 'mynewpass',
-      });
+    const objectStub = {
+      id: 1,
+      email: 'teste@teste.com',
+    };
+    const stub = sinon.stub(loginService, 'login').resolves(objectStub);
     const response = await request(app)
       .post('/login')
-      .send({ email: 'newteste@teste.com', password: 'mynewpass' });
+      .send({ email: 'teste@teste.com', password: 'mynewpass' });
     expect(response.status).to.eql(200);
+    expect(response.body).to.be.a('string');
+    stub.restore();
   });
 });
---
+++ src/tests/unit/services/login.service.spec.ts
@@ -0,0 +1,50 @@
+// import request from 'supertest';
+// import sinon from 'sinon';
+// import { expect } from 'chai';
+// import app from '../../../app';
+// import UserModel from '../../../models/UserModel';
+// import HttpException from '../../../utils/http.exception';
+// import HttpStatusCode from '../../../utils/http.status.code';
+
+// describe('Test login controller', () => {
+//   it('Test login without sending data', async () => {
+//     const response = await request(app)
+//       .post('/login')
+//       .send({});
+//     expect(response.status).to.eql(400);
+//   });
+//   it('Test login sending incorrect email format', async () => {
+//     const response = await request(app)
+//       .post('/login')
+//       .send({ email: 'incorretEmailFormat', password: 'password' });
+//     expect(response.status).to.eql(422);
+//   });
+//   it('Test login sending short password', async () => {
+//     const response = await request(app)
+//       .post('/login')
+//       .send({ email: 'test@test.com', password: 'short' });
+//     expect(response.status).to.eql(422);
+//   });
+//   it('Test login sending invalid credentials', async () => {
+//     const stub = sinon.stub(loginService, 'login').throws(new HttpException(HttpStatusCode.UNAUTHORIZED, 'Invalid Credentials'));
+//     const response = await request(app)
+//       .post('/login')
+//       .send({ email: 'teste@teste.com', password: 'newpassword' });
+//     expect(response.status).to.eql(401);
+//     expect(response.body.message).to.eql('Invalid Credentials');
+//     stub.restore();
+//   });
+//   it('Test login successful', async () => {
+//     const objectStub = {
+//       id: 1,
+//       email: 'teste@teste.com',
+//     };
+//     const stub = sinon.stub(loginService, 'login').resolves(objectStub);
+//     const response = await request(app)
+//       .post('/login')
+//       .send({ email: 'teste@teste.com', password: 'mynewpass' });
+//     expect(response.status).to.eql(200);
+//     expect(response.body).to.be.a('string');
+//     stub.restore();
+//   });
+// });
