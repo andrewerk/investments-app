ffd580e15652120071cc666dd9fb91c571c00f05
Andre Schevz de Werk <andrewerk1804@gmail.com>
2022-07-24 13:51:14 -0300

    docs(swagger): updating swagger with information about earnings and historic endpoints

--- src/controllers/investmentPortfolioController.ts
+++ src/controllers/investmentPortfolioController.ts
@@ -21,14 +21,14 @@ const getEarningByStock = async (req: Request, res: Response): Promise<Response>
   const { stock } = req.params;
   const earnings = await investmentsPortfolioService
     .getEarningByStock(id, stock.toUpperCase());
-  return res.status(HttpStatusCode.OK).json(earnings);
+  return res.status(HttpStatusCode.OK).json({ earnings });
 };
 
 const getTotalEarnings = async (req: Request, res: Response): Promise<Response> => {
   const { id } = res.locals.user;
   const earnings = await investmentsPortfolioService
     .getTotalEarnings(id);
-  return res.status(HttpStatusCode.OK).json(earnings);
+  return res.status(HttpStatusCode.OK).json({ earnings });
 };
 
 export default {
